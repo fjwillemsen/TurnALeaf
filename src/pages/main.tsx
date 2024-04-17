88575a8a11fa8135519a3966a30670c8f891af5b
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2024-04-17 12:18:30 +0200

    Improvements to responsive layout

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,5 @@
 import { lazy, Component } from 'react'
-import { Responsive, WidthProvider} from 'react-grid-layout'
+import { Responsive, WidthProvider } from 'react-grid-layout'
 
 import '../../node_modules/react-grid-layout/css/styles.css'
 import '../../node_modules/react-resizable/css/styles.css'
@@ -11,36 +11,36 @@ const FullGridLayout = WidthProvider(Responsive);
 class Grid extends Component {
   render() {
     // layout is an array of objects, see the demo for more complete usage
+    const layout = [
+        { i: "files", x: 0, y: 0, w: 2, h: 1, static: true },
+        { i: "editor", x: 2, y: 0, w: 5, h: 1, minW: 2, maxW: 10, minH: 1, maxH: 1 },
+        { i: "pdf", x: 7, y: 0, w: 5, h: 1, minH: 1, maxH: 1 }
+    ]
     const layouts = {
-        lg: [
-            { i: "files", x: 0, y: 0, w: 2, h: 1, static: true },
-            { i: "editor", x: 2, y: 0, w: 5, h: 1, minW: 2, maxW: 10 },
-            { i: "pdf", x: 7, y: 0, w: 5, h: 1 }
-        ],
-        xxs: [
-            { i: "files", x: 0, y: 0, w: 2, h: 1, static: true },
-            { i: "editor", x: 2, y: 0, w: 5, h: 1, minW: 2, maxW: 10 },
-            { i: "pdf", x: 7, y: 0, w: 5, h: 1 }
-        ]
+        lg: layout,
+        xxs: layout
     };
     return (
       <FullGridLayout
         className="layout"
         layouts={layouts}
+        rowHeight={document.body.clientHeight}
+        maxRows={1}
+        compactType={'horizontal'}
         cols={{lg: 12, md: 12, sm: 12, xs: 12, xxs: 12}}
         margin={[20, 20]}
       >
-        <div key="files">
+        <div key="files" style={{border: "1px solid green"}}>
             <ul>
                 <li>file</li>
                 <li>goes</li>
                 <li>here</li>
             </ul>
         </div>
-        <div key="editor" style={{backgroundColor: 'red'}}>
+        <div key="editor" style={{border: "1px solid red"}}>
             <p>Hello world</p>
         </div>
-        <div key="pdf">
+        <div key="pdf" style={{border: "1px solid blue"}}>
             <Preview />
         </div>
       </FullGridLayout>
