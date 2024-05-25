728e9df6668d44c19f80c84e487dcbd6836425ce
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2024-05-25 20:44:09 +0200

    Added TSDoc plugin and automatic docstring generation

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,19 @@ import tseslint from 'typescript-eslint'
 import pluginReactConfig from 'eslint-plugin-react/configs/jsx-runtime.js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import gitignore from 'eslint-config-flat-gitignore'
+import tsdoc from 'eslint-plugin-tsdoc'
 
 export default [
     gitignore(),
     {
         languageOptions: { globals: globals.browser },
         ignores: ['.github/'],
+        plugins: {
+            tsdoc,
+        },
+        rules: {
+            'tsdoc/syntax': 'error',
+        },
     },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
