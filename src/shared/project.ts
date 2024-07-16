148a5a92324005bb774f61ceaa3cdb256ebb1fc9
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2024-07-16 13:37:01 +0200

    Improved function descriptions

--- src/shared/project.ts
+++ src/shared/project.ts
@@ -10,7 +10,7 @@ export abstract class AbstractProjectID {
     /**
      * Creates an instance of ProjectID.
      *
-     * @param url - the URL to the project
+     * @param url - the URL to the project.
      */
     constructor(url: URL) {
         this._url_string = url.toString()
@@ -20,36 +20,36 @@ export abstract class AbstractProjectID {
     /**
      * Makes a hash out of the URL.
      *
-     * @param url - the URL to hash
-     * @returns string - the hash
+     * @param url - the URL to hash.
+     * @returns string - the hash.
      */
     protected abstract make_hash(url: URL): string
 
     /**
      * Gets the hash identifying this project.
      *
-     * @returns string - the hash identifyer
+     * @returns string - the hash identifyer.
      */
     abstract get hash(): string
 
     /**
      * Gets the URL to the remote repository this project belongs to.
      *
-     * @returns URL - the URL of the remote repository
+     * @returns URL - the URL of the remote repository.
      */
     abstract get url(): URL
 
     /**
-     * Gets the project directory on disk
+     * Gets the project directory on disk.
      *
-     * @returns string - absolute path to the directory
+     * @returns string - absolute path to the directory.
      */
     abstract get directory(): string
 
     /**
      * Checks whether the project directory exists on disk.
      *
-     * @returns boolean - whether the directory exists
+     * @returns boolean - whether the directory exists.
      */
     abstract exists_dir(): boolean
 
@@ -108,7 +108,7 @@ export abstract class AbstractProject {
     /**
      * Helper function to retrieve the name during construction.
      *
-     * @returns string - the name of the project
+     * @returns string - the name of the project.
      */
     protected abstract get_name(): Promise<string>
 
@@ -122,26 +122,28 @@ export abstract class AbstractProject {
     /**
      * Sets the name of the project.
      *
-     * @param string - the name of the project
+     * @param string - the name of the project.
      */
     abstract set name(name: string)
 
     /**
      * Gets the ProjectID of this project.
      *
-     * @returns AbstractProjectID - the ProjectID
+     * @returns AbstractProjectID - the ProjectID.
      */
     abstract get id(): AbstractProjectID
 
     /**
      * Fetch updates to the project from remote.
      *
+     * @returns boolean - true if an update is available, false otherwise.
      */
     abstract get_project_update(): Promise<boolean>
 
     /**
      * Push updates of the project to remote.
      *
+     * @returns string | void - the SHA of the commit.
      */
     abstract push_project_update(): Promise<string | void>
 
